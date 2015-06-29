5a147eeb46dcf8664d8a88f5388c9d410537a61b
Nanda Ballabh <nandaballabh.kec08@gmail.com>
2015-06-29 17:04:30 +0530

    Initial Commit

---
+++ iOSInBuildScanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSInBuildScanner
+//
+//  Created by Nanda Ballabh on 6/29/15.
+//  Copyright (c) 2015 Nanda Ballabh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSInBuildScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSInBuildScanner
+//
+//  Created by Nanda Ballabh on 6/29/15.
+//  Copyright (c) 2015 Nanda Ballabh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
