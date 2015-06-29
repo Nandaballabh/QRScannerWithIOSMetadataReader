8022327d82e8df614a8bc748e089e8494bb6c978
Nanda Ballabh <nandaballabh.kec08@gmail.com>
2015-06-29 17:29:24 +0530

    Project pushed

---
+++ iOSInBuildScanner/NBScanner.h
@@ -0,0 +1,13 @@
+//
+//  NBScanner.h
+//  iOSInBuildScanner
+//
+//  Created by Nanda Ballabh on 6/29/15.
+//  Copyright (c) 2015 Nanda Ballabh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NBScanner : NSObject
+
+@end
---
+++ iOSInBuildScanner/NBScanner.m
@@ -0,0 +1,13 @@
+//
+//  NBScanner.m
+//  iOSInBuildScanner
+//
+//  Created by Nanda Ballabh on 6/29/15.
+//  Copyright (c) 2015 Nanda Ballabh. All rights reserved.
+//
+
+#import "NBScanner.h"
+
+@implementation NBScanner
+
+@end
